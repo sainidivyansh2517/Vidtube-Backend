2dc948161995e66d003c9786f30a9ca8e3fb62c8
Divyansh Saini <divyanshsainijnvsre@gmail.com>
2026-01-04 22:20:53 +0530

    login user controller established

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,6 +6,23 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
+
+const generateAccessAndRefreshTokens = async(userId) => {
+  try {
+    const user = await User.findById(userId)
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken
+    await user.save({validateBeforeSave: false})
+
+    return {accessToken, refreshToken}
+
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating tokens")
+  }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
@@ -79,5 +96,62 @@ const registerUser = asyncHandler( async (req, res) => {
     )
 
 } )
+
+
+const logiUser = asyncHandler(async (req, res) => {
+  // req -> data
+  // username or email
+  // find one user
+  // password check
+  // assess and refresh token 
+  // send cookies
+
+
+
+  const {email, username, password} = req.body
+
+  if(!username || !email) {
+    throw new ApiError(400, "Username or email is required")
+  }
+
+  const user = await User.findOne({
+    $or: [{username}, {email}]
+  })
+
+  if(!user){
+    throw new ApiError(404, "User does not exist")
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password)
+
+  if(!isPasswordValid){
+    throw new ApiError (401, "Invalid user credential")
+  }
+
+  const loggedInUser = await user.findById(user._id).select("-password -refreshToken")
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(
+      200,
+      {
+        user: loggedInUser, accessToken,
+        refreshToken
+      },
+      "User logged In Successfully"
+    )
+  )
+
+  
+})
+
+
 
 export { registerUser }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,6 +43,9 @@ const userSchema = new Schema(
             type: String,
             required: [true, 'Password is required']
         },
+        refreshToken: {
+            type: String,
+        }
     },
     {timestamps: true}
 )
